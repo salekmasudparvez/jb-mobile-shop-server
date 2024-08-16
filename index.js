b0cd4e8349c7b65da57d825264050b672f065870
Salek Masud Parvez <151811650+salekmasudparvez@users.noreply.github.com>
2024-08-16 15:54:21 +0600

    added basic setup

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require("cors");
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5000",
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+const app = express();
+app.use(cors(corsOptions));
+app.use(express.json());
+
+const uri = process.env.VITE_uri;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
