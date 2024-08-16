402f93a687ddda1ab2c17db78838c260d563b3bf
Salek Masud Parvez <151811650+salekmasudparvez@users.noreply.github.com>
2024-08-16 23:45:04 +0600

    added:product and image update routes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const axios = require('axios');
+const multer = require('multer');
 const cors = require("cors");
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -15,6 +17,7 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 const app = express();
+const upload = multer(); 
 app.use(cors(corsOptions));
 app.use(express.json());
 
@@ -31,11 +34,53 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const db = client.db("mobileShopDB");
+    const usersCollection = db.collection('users');
+    const productCollection = db.collection('products');
     
+    app.post('/register',async(req,res)=>{
+      const newUser = req.body;
+      const existingUser = await usersCollection.findOne({ name: newUser?.name});
+      if (existingUser) {
+        return res.status(400).json({ message: 'Username already exists' });
+      }
+      const result = await usersCollection.insertOne(newUser);
+      res.json(result)
+    })
+    app.get('/users/:email',async(req,res)=>{
+      const email = req.params.email;
+      const users = await usersCollection.findOne({email});
+      res.json(users);
+    })
+    app.post('/imageUpload', upload.single('image'), async (req, res) => {
+      const imageBuffer = req.file.buffer;
+      //console.log(imageBuffer,'line57')
+      try {
+        const response = await axios.post('https://api.imgur.com/3/image', imageBuffer, {
+          headers: {
+            Authorization: `Client-ID ${process.env.VITE_imgurClientId}`,
+            'Content-Type': 'image/jpeg'
+          }
+        });
+          
+        const imageUrl = response.data.data.link;
+        res.json({ imageUrl });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Image upload failed' });
+      }
+    });
+    
+    app.post('/productUpload',async(req,res)=>{
+      const product = req.body;
+      console.log(product)
+      const result = await productCollection.insertOne(product);
+      res.json(result);
+    })
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -43,3 +88,7 @@ async function run() {
   }
 }
 run().catch(console.dir);
+app.listen(port, () => {
+  console.log(`Mobile shop server is running on port: ${port}`);
+});
+
